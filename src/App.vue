a31fae828f7393a27dd14e5be3efe8cb1b6e70c2
laveena95 <laveenalavee256@gmail.com>
2020-08-02 14:53:04 +0530

    reference component

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,27 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+   <ReasonComponent/>
+   <ReferenceComponent/>
+   <CalComponent/>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import ReasonComponent from './components/ReasonComponent';
+import ReferenceComponent from './components/ReferenceComponent';
+import CalComponent from './components/CalComponent';
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: 'App',
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  components: {
+    ReasonComponent,
+    ReferenceComponent,
+    CalComponent,
+  },
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/ReferenceComponent.vue
@@ -0,0 +1,87 @@
+<template>
+<v-container fluid>
+    <h1 class="text-center font-weight-regular">CHECK OUR REFERENCES</h1>
+        <h2 class="body-1 text-center">
+            Our clientele includes a diverse group of partnerships, limited liability companies, associations and foundations, and our customer turnover is limited.
+        </h2>
+    <v-row class="ma-10 ">
+        <v-col cols="12" md="4" v-for="item in items" :key="item.id">
+                <v-hover v-slot:default="{ hover }">
+                    <v-card
+                    class="mx-auto"
+                    color="grey lighten-4"
+                    >
+                    <v-img
+                        :aspect-ratio="16/9"
+                        :src="item.img"
+                    >
+                        <v-expand-transition>
+                            
+                        <div
+                            v-if="hover"
+                            class="d-flex transition-fast-in-fast-out white darken-2 v-card--reveal display-3 black--text ma-2"
+                            style="height: 94%;"
+                        >
+                            <p class="body-1 text-center" absolute> {{ item.text}} </p>
+                        </div>
+                        </v-expand-transition>
+                    </v-img>
+                    </v-card>
+                </v-hover>
+        </v-col>
+    </v-row>
+</v-container>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                items: [
+                    {
+                        id: '1',
+                        img: require('../assets/img/1.jpg'),
+                        text: 'Loreum ipsum',
+                    },
+                    {
+                        id: '2',
+                        img: require('../assets/img/2.jpg'),
+                        text: 'Loreum ipsum',
+                    },
+                    {
+                        id: '3',
+                        img: require('../assets/img/3.jpg'),
+                        text: 'Loreum ipsum',
+                    },
+                    {
+                        id: '4',
+                        img: require('../assets/img/4.jpg'),
+                        text: 'Loreum ipsum',
+                    },
+                    {
+                        id: '5',
+                        img: require('../assets/img/5.jpg'),
+                        text: 'Loreum ipsum',
+                    },
+                    {
+                        id: '6',
+                        img: require('../assets/img/6.jpg'),
+                        text: 'Loreum ipsum',
+                    }
+                ]
+            }
+        }
+    }
+</script>
+
+
+<style>
+.v-card--reveal {
+  align-items: center;
+  bottom: 0;
+  justify-content: center;
+  opacity: .7;
+  position: absolute;
+  width: 100%;
+}
+</style>
